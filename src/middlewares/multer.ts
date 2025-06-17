74b0e29cda7f1015c0df7ca9a2a672afbb13a7ae
f-zamanan <f.n.zamanan@gmail.com>
2025-06-17 20:18:50 +0300

    did the filtering task, error handling still 404

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,4 +1,6 @@
+import { Request } from "express";
 import multer from "multer";
+import path from "path";
 
 // this middleware is router lever, why? because i only need image/files upload in certain endpoints
 // this will store the uploaded files in the /tmp/my-uploads directory
@@ -13,7 +15,33 @@ const storage = multer.diskStorage({
     cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
   },
 });
+// const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
+//   // The function should call `cb` with a boolean
+//   // to indicate if the file should be accepted
+
+//   // To accept only .png files
+//   if (file.mimetype === "image/png") {
+//     cb(null, true); // this will accept the file?
+//   } else {
+//     cb(Error("only .PNG files accepted"), false);
+//   }
+// };
 // task: restrict the user to .png file type
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = /jpg/;
+    const extValid = allowedTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimeValid = allowedTypes.test(file.mimetype);
+
+    if (extValid && mimeValid) {
+      cb(null, true); // Accept the file
+    } else {
+      cb(new Error("Only .jpg files are allowed!") as any, false); // Reject the file
+    }
+  },
+});
 
 export default upload;
